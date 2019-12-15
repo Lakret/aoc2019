d80ab58794eee4a00234fe773b29154c8f8f706c
Lakret <Rakuretu@gmail.com>
2019-12-15 17:01:08 +0100

    d15p2

--- lib/aoc2019/day15.ex
+++ lib/aoc2019/day15.ex
@@ -3,9 +3,17 @@ defmodule Aoc2019.Day15 do
 
   alias Aoc2019.Intcode
 
-  @spec solve_part_one() :: Intcode.value()
+  @spec solve_part_one() :: non_neg_integer()
   def solve_part_one() do
-    flood_fill()
+    {steps_taken, _location_map} = flood_fill()
+    steps_taken
+  end
+
+  @spec solve_part_two :: non_neg_integer()
+  def solve_part_two() do
+    {_steps_taken, location_map} = flood_fill(false)
+    {steps_taken, _oxygenated} = flood_with_oxygen(location_map)
+    steps_taken - 1
   end
 
   # Helpers
@@ -20,29 +28,77 @@ defmodule Aoc2019.Day15 do
   @type location_map :: %{position() => status()}
 
   @type thread_state :: {status, Intcode.input_cont()}
+  @type thread_states_with_pos :: [{thread_state(), position()}]
 
   @possible_directions ~w(n s w e)a
 
-  def flood_fill() do
-    init_threads_with_position = Enum.map(@possible_directions, &move_with_position(&1))
+  @spec flood_with_oxygen(location_map()) :: {steps_taken :: non_neg_integer(), MapSet.t()}
+  def flood_with_oxygen(location_map) do
+    {oxygenator_position, :target} = Enum.find(location_map, fn {_position, status} -> status == :target end)
+    init_threads_with_position = Enum.map(@possible_directions, &move_on_map(&1, oxygenator_position, location_map))
+    oxygenated = MapSet.new([oxygenator_position])
 
-    advance_fill(init_threads_with_position, 1, %{})
+    advance_fill_oxygen(init_threads_with_position, 1, location_map, oxygenated)
   end
 
-  def advance_fill(thread_states_with_pos, steps_taken, location_map) do
-    # threads_count = length(thread_states_with_pos)
-    # map_locations = Map.keys(location_map) |> length()
-    # IO.puts("steps_taken: #{steps_taken}, threads count: #{threads_count}, map locations: #{map_locations}")
+  @spec advance_fill_oxygen([{position(), status()}], non_neg_integer(), location_map(), MapSet.t()) ::
+          {steps_taken :: non_neg_integer(), oxygenated :: MapSet.t()}
+  def advance_fill_oxygen(positions_to_status, steps_taken, location_map, oxygenated) do
+    threads_count = length(positions_to_status)
 
-    if target_hit?(thread_states_with_pos) do
-      steps_taken
+    oxygenated =
+      Enum.reduce(positions_to_status, oxygenated, fn {position, _status}, oxygenated ->
+        MapSet.put(oxygenated, position)
+      end)
+
+    if threads_count == 0 do
+      {steps_taken, oxygenated}
+    else
+      positions_to_status =
+        Enum.flat_map(positions_to_status, fn
+          {_position, :wall} ->
+            []
+
+          {position, _} ->
+            Enum.reject(@possible_directions, fn direction ->
+              MapSet.member?(oxygenated, new_position(position, direction))
+            end)
+            |> Enum.map(&move_on_map(&1, position, location_map))
+        end)
+
+      advance_fill_oxygen(positions_to_status, steps_taken + 1, location_map, oxygenated)
+    end
+  end
+
+  @spec move_on_map(direction(), position(), location_map()) :: {position(), status()}
+  def move_on_map(direction, position, location_map) do
+    position = new_position(position, direction)
+    {position, Map.fetch!(location_map, position)}
+  end
+
+  @spec flood_fill(boolean()) :: {steps_taken :: non_neg_integer(), location_map()}
+  def flood_fill(terminate_on_target_hit \\ true) do
+    init_threads_with_position = Enum.map(@possible_directions, &move_with_position(&1))
+
+    advance_fill(init_threads_with_position, 1, %{}, terminate_on_target_hit)
+  end
+
+  @spec advance_fill(thread_states_with_pos(), non_neg_integer(), location_map(), boolean()) ::
+          {steps_taken :: non_neg_integer(), location_map()}
+  def advance_fill(thread_states_with_pos, steps_taken, location_map, terminate_on_target_hit) do
+    threads_count = length(thread_states_with_pos)
+
+    if (terminate_on_target_hit && target_hit?(thread_states_with_pos)) || threads_count == 0 do
+      location_map = update_location_map(location_map, thread_states_with_pos)
+
+      {steps_taken, location_map}
     else
       thread_states_with_pos =
         Enum.flat_map(thread_states_with_pos, fn
           {{:wall, _input_cont}, _position} ->
             []
 
-          {{:ok, input_cont}, position} ->
+          {{_, input_cont}, position} ->
             case location_map[position] do
               :wall ->
                 []
@@ -55,20 +111,24 @@ defmodule Aoc2019.Day15 do
             end
         end)
 
-      location_map =
-        Enum.reduce(thread_states_with_pos, location_map, fn {{status, _cont}, position}, location_map ->
-          Map.put(location_map, position, status)
-        end)
+      location_map = update_location_map(location_map, thread_states_with_pos)
 
-      advance_fill(thread_states_with_pos, steps_taken + 1, location_map)
+      advance_fill(thread_states_with_pos, steps_taken + 1, location_map, terminate_on_target_hit)
     end
   end
 
-  @spec target_hit?([{thread_state(), position()}]) :: boolean
+  @spec target_hit?(thread_states_with_pos()) :: boolean
   def target_hit?(thread_states_with_position) do
     Enum.any?(thread_states_with_position, fn
       {{:target, _}, _} -> true
       _ -> false
+    end)
+  end
+
+  @spec update_location_map(location_map(), thread_states_with_pos()) :: location_map()
+  def update_location_map(location_map, thread_states_with_pos) do
+    Enum.reduce(thread_states_with_pos, location_map, fn {{status, _cont}, position}, location_map ->
+      Map.put(location_map, position, status)
     end)
   end
 
@@ -113,29 +173,6 @@ defmodule Aoc2019.Day15 do
       end
 
     {decode_status(output), input_cont}
-  end
-
-  def move_manually() do
-    read_input(15)
-    |> Intcode.execute_program_with_io_adapter(
-      [],
-      fn ->
-        direction =
-          case IO.gets("next move?\n") |> String.trim() do
-            "n" -> 1
-            "s" -> 2
-            "w" -> 3
-            "e" -> 4
-            "stop" -> raise "Movement program stopped."
-          end
-
-        direction
-      end,
-      fn status ->
-        status = decode_status(status)
-        IO.puts("#{status}")
-      end
-    )
   end
 
   @spec decode_status(status_raw()) :: status()
