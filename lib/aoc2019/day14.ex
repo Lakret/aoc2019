1d85dfd108712c02d0cbb38c653e6cde2c05a099
Lakret <Rakuretu@gmail.com>
2019-12-15 21:38:04 +0100

    d14 p1

--- lib/aoc2019/day14.ex
+++ lib/aoc2019/day14.ex
@@ -1,11 +1,16 @@
 defmodule Aoc2019.Day14 do
   import Aoc2019
 
+  require Logger
+
   @spec solve_part_one() :: non_neg_integer()
   def solve_part_one() do
-    read_input(14)
-    |> parse_reactions()
-    |> needed_ore("FUEL", 1)
+    {ore, _unused} =
+      read_input(14)
+      |> parse_reactions()
+      |> find_total_ore("FUEL", 1)
+
+    ore
   end
 
   # Parser
@@ -42,8 +47,46 @@ defmodule Aoc2019.Day14 do
     {compound, quantity}
   end
 
-  @spec needed_ore(reactions(), compound(), quantity()) :: quantity()
-  def needed_ore(reactions, compound, quantity) do
-    0
+  # Solver
+
+  @ore "ORE"
+
+  @type unused_compounds :: %{compound => quantity}
+
+  @spec find_total_ore(reactions(), compound(), quantity(), unused_compounds()) :: {quantity(), unused_compounds()}
+  def find_total_ore(reactions, compound, quantity, unused_compounds \\ %{}) do
+    unused_quantity = unused_compounds[compound] || 0
+
+    if unused_quantity >= quantity do
+      still_unused = unused_quantity - quantity
+      Logger.debug("to get #{quantity} #{compound}: using #{quantity} of unused (#{still_unused} still unused).")
+
+      {0, Map.put(unused_compounds, compound, still_unused)}
+    else
+      %{reagents: reagents, yield: yield} = Map.fetch!(reactions, compound)
+
+      quantity_to_synthesize = quantity - unused_quantity
+      times = ceil(quantity_to_synthesize / yield)
+      unused = times * yield - quantity_to_synthesize
+      Logger.debug("(before reduce) unused: #{inspect(unused_compounds)}, current unused: #{unused}")
+
+      {total_ore, unused_compounds} =
+        Enum.reduce(reagents, {0, unused_compounds}, fn
+          {@ore, ore_quantity}, {total_ore, unused_compounds} ->
+            {total_ore + times * ore_quantity, unused_compounds}
+
+          {compound, quantity}, {total_ore, unused_compounds} ->
+            {add_ore, unused_compounds} = find_total_ore(reactions, compound, times * quantity, unused_compounds)
+            Logger.debug("(in reduce) new unused: #{inspect(unused_compounds)}")
+
+            {total_ore + add_ore, unused_compounds}
+        end)
+
+      unused_compounds = Map.put(unused_compounds, compound, unused)
+
+      Logger.debug("to get #{quantity} #{compound}: #{times}x #{inspect(reagents)}")
+      Logger.debug("\tunused compounds after: #{inspect(unused_compounds)}")
+      {total_ore, unused_compounds}
+    end
   end
 end
--- test/aoc2019/day14_test.exs
+++ test/aoc2019/day14_test.exs
@@ -1,34 +1,46 @@
 defmodule Aoc2019.Day14Test do
   use ExUnit.Case
 
-  alias Aoc2019.Day14
+  import Aoc2019.Day14
 
+  # 577_601 is wrong
+  # 439699 is too low
   test "part 1 is solved correctly" do
-    assert Day14.solve_part_one() != 0
+    assert solve_part_one() == 504_284
   end
 
   # test "part 2 is solved correctly" do
-  #   assert Day14.solve_part_two() == 394
+  #   assert solve_part_two() == 394
   # end
 
-  test "reactions are parsed correctly" do
-    reactions =
-      Day14.parse_reactions("""
-      10 ORE => 10 A
-      1 ORE => 1 B
-      7 A, 1 B => 1 C
-      7 A, 1 C => 1 D
-      7 A, 1 D => 1 E
-      7 A, 1 E => 1 FUEL
-      """)
+  @simple_reactions_raw """
+  10 ORE => 10 A
+  1 ORE => 1 B
+  7 A, 1 B => 1 C
+  7 A, 1 C => 1 D
+  7 A, 1 D => 1 E
+  7 A, 1 E => 1 FUEL
+  """
 
-    assert reactions == %{
-             "A" => %{reagents: [{"ORE", 10}], yield: 10},
-             "B" => %{reagents: [{"ORE", 1}], yield: 1},
-             "C" => %{reagents: [{"A", 7}, {"B", 1}], yield: 1},
-             "D" => %{reagents: [{"A", 7}, {"C", 1}], yield: 1},
-             "E" => %{reagents: [{"A", 7}, {"D", 1}], yield: 1},
-             "FUEL" => %{reagents: [{"A", 7}, {"E", 1}], yield: 1}
-           }
+  @simple_reactions %{
+    "A" => %{reagents: [{"ORE", 10}], yield: 10},
+    "B" => %{reagents: [{"ORE", 1}], yield: 1},
+    "C" => %{reagents: [{"A", 7}, {"B", 1}], yield: 1},
+    "D" => %{reagents: [{"A", 7}, {"C", 1}], yield: 1},
+    "E" => %{reagents: [{"A", 7}, {"D", 1}], yield: 1},
+    "FUEL" => %{reagents: [{"A", 7}, {"E", 1}], yield: 1}
+  }
+
+  test "reactions are parsed correctly" do
+    assert parse_reactions(@simple_reactions_raw) == @simple_reactions
+  end
+
+  test "simple reactions ore calculation is correct" do
+    assert find_total_ore(@simple_reactions, "A", 30) == {30, %{"A" => 0}}
+    assert find_total_ore(@simple_reactions, "A", 28) == {30, %{"A" => 2}}
+    assert find_total_ore(@simple_reactions, "A", 20) == {20, %{"A" => 0}}
+
+    assert find_total_ore(@simple_reactions, "FUEL", 1) ==
+             {31, %{"A" => 2, "B" => 0, "C" => 0, "D" => 0, "E" => 0, "FUEL" => 0}}
   end
 end
