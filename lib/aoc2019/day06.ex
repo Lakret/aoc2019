bb17e00216007e17a316e321ca124175fb44f723
Lakret <Rakuretu@gmail.com>
2019-12-22 22:11:32 +0100

    d6p1

--- lib/aoc2019/day06.ex
+++ lib/aoc2019/day06.ex
@@ -1,39 +1,41 @@
 defmodule Aoc2019.Day06 do
   import Aoc2019
 
+  require Logger
+
   def solve_part_one() do
+    read_input(6)
+    |> parse()
+    |> all_orbits()
+    |> MapSet.size()
   end
 
   # Helpers
 
   @doc ~S"""
-    import Aoc2019
-    alias Aoc2019.Day06
+  Parses the direct orbits relations input. E.g., this input:
 
-    input = "
-    COM)B
-    B)C
-    C)D
-    D)E
-    E)F
-    B)G
-    G)H
-    D)I
-    E)J
-    J)K
-    K)L
-    "
+  ```
+  COM)B
+  B)C
+  C)D
+  D)E
+  E)F
+  B)G
+  G)H
+  D)I
+  E)J
+  J)K
+  K)L
+  ```
 
-    [directly] orbits relation:
+  denotes the following [directly] orbits relation:
 
-    COM, B -> COM, C -> B, D -> C, E -> D, F -> E, G -> B, ...
-
-    orbits relation is quasi-transitive, thus:
-
-    D -(orbits)-> C, C -(orbits)-> B => D -(indirectly orbits)-> B
+  ```
+  COM, B -> COM, C -> B, D -> C, E -> D, F -> E, G -> B, ...
+  ```
 
   """
-
   def parse(input) do
     input
     |> String.trim()
@@ -42,15 +44,51 @@ defmodule Aoc2019.Day06 do
     |> Enum.reduce(%{}, &add_to_graph/2)
   end
 
-  # B)A => a directly orbits b
-  def parse_relation(relation) do
+  defp parse_relation(relation) do
     [b, a] = String.split(relation, ")")
     {a, b}
   end
 
-  def add_to_graph({a, b}, graph) do
-    Map.update(graph, a, %{orbits: [b]}, fn relations ->
-      Map.put(relations, :orbits, [b | relations.orbits])
+  defp add_to_graph({a, b}, graph) do
+    Map.update(graph, a, [b], fn relations ->
+      [b | relations.orbits]
+    end)
+  end
+
+  @doc """
+  Returns a set of all direct & indirect orbits in `orbits` graph.
+
+  Note that orbits relation is quasi-transitive, e.g.:
+
+  ```
+  D -(orbits)-> C, C -(orbits)-> B => D -(indirectly orbits)-> B
+  ```
+  """
+  def all_orbits(orbits) do
+    orbits
+    |> Enum.with_index()
+    |> Enum.reduce(MapSet.new(), fn {{a, bs}, idx}, all_orbits ->
+      Logger.info("processing #{idx}")
+
+      Enum.reduce(bs, all_orbits, fn b, all_orbits ->
+        MapSet.put(all_orbits, {a, b})
+        |> add_indirect_orbits(a, b, orbits)
+      end)
+    end)
+  end
+
+  defp add_indirect_orbits(all_orbits, a, b, orbits) do
+    cs = orbits[b] || []
+    Logger.debug("orbits[#{inspect(b)}] = #{inspect(cs)}")
+
+    Enum.reduce(cs, all_orbits, fn c, all_orbits ->
+      # if relation a -([indirectly] orbits)-> c has already been traced,
+      # we don't need to re-trace it again.
+      indirect_orbits_of_a_through_c = add_indirect_orbits(all_orbits, a, c, orbits)
+      Logger.debug("adding #{a} -(indirectly orbits)-> #{c} relation, and tracing from #{a} through #{c}...")
+
+      MapSet.put(all_orbits, {a, c})
+      |> MapSet.union(indirect_orbits_of_a_through_c)
     end)
   end
 end
--- test/aoc2019/day06_test.exs
+++ test/aoc2019/day06_test.exs
@@ -3,6 +3,10 @@ defmodule Aoc2019.Day16Test do
 
   import Aoc2019.Day06
 
+  test "part 1 is solved correctly" do
+    assert solve_part_one() == 301_100
+  end
+
   @test_orbits """
   COM)B
   B)C
@@ -21,19 +25,32 @@ defmodule Aoc2019.Day16Test do
     actual = parse(@test_orbits)
 
     expected = %{
-      "B" => %{orbits: ["COM"]},
-      "C" => %{orbits: ["B"]},
-      "D" => %{orbits: ["C"]},
-      "E" => %{orbits: ["D"]},
-      "F" => %{orbits: ["E"]},
-      "G" => %{orbits: ["B"]},
-      "H" => %{orbits: ["G"]},
-      "I" => %{orbits: ["D"]},
-      "J" => %{orbits: ["E"]},
-      "K" => %{orbits: ["J"]},
-      "L" => %{orbits: ["K"]}
+      "B" => ["COM"],
+      "C" => ["B"],
+      "D" => ["C"],
+      "E" => ["D"],
+      "F" => ["E"],
+      "G" => ["B"],
+      "H" => ["G"],
+      "I" => ["D"],
+      "J" => ["E"],
+      "K" => ["J"],
+      "L" => ["K"]
     }
 
     assert actual == expected
+  end
+
+  test "all_orbits works" do
+    orbits = parse(@test_orbits) |> all_orbits()
+
+    assert {"B", "COM"} in orbits
+    assert {"C", "COM"} in orbits
+    assert {"D", "COM"} in orbits
+    assert {"D", "B"} in orbits
+    assert {"B", "D"} not in orbits
+    assert {"COM", "B"} not in orbits
+
+    assert MapSet.size(orbits) == 42
   end
 end
